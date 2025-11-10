5f8a730fc3163d3e2c5c010436c5cdf683b14c29
Hussein <hussein.beshir100@gmail.com>
2025-11-11 01:21:18 +0300

    Update message routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./modules/auth/auth.route";
 import roomRoutes from "./modules/rooms/room.route";
 import proofRoutes from "./modules/proofs/proof.route";
+import messageRoutes from "./modules/messages/message.route";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/rooms", roomRoutes);
 app.use("/api/proofs", proofRoutes);
+app.use("/api/messages", messageRoutes);
 
 export default app;
